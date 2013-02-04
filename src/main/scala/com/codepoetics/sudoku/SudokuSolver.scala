cc268032e8d6b1ac22c6b675903962b7cd68a212
Dominic Fox <dominic.fox@gmail.com>
2013-02-04 16:40:28 +0000

    Blow up if we cross our tracks

--- src/main/scala/com/codepoetics/sudoku/SudokuSolver.scala
+++ src/main/scala/com/codepoetics/sudoku/SudokuSolver.scala
@@ -1,7 +1,17 @@
 package com.codepoetics.sudoku
 
+import com.codepoetics.sudoku.Types.PlacedDigit
+
 object SudokuSolver {
+
+  var previousStates = Set.empty[Set[PlacedDigit]]
   def solve(state: SudokuState): Option[SudokuState] = {
+    val placedDigits = state.placedDigits.toSet
+    if (previousStates.contains(placedDigits)) throw new Exception("We have been here before")
+    previousStates  += placedDigits.toSet
+
+    println(previousStates.size + "\n")
+
     if (!state.isValid || !state.isCompletable) None
     else if (state.isComplete) Some(state)
     else if (state.hasForcedMoves) state.afterForced.flatMap { newState => solve(newState) }
